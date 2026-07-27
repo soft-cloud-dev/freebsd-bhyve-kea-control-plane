1b05cbacebfc58d6512576e2bc8b1159db76c2df
Lukasz D. Tulikowski <lukasz@softcloud.dev>
2026-07-27 19:28:57 +0200

    Install DHCPv4-only keactrl configuration

--- scripts/02_install_dependencies.sh
+++ scripts/02_install_dependencies.sh
@@ -23,11 +23,15 @@ pkg install -y \
     tmux \
     vm-bhyve
 
+install -d -m 0750 /usr/local/etc/kea
+install -m 0640 config/keactrl.conf /usr/local/etc/kea/keactrl.conf
+
 sysrc zfs_enable=YES >/dev/null
 sysrc vm_enable=YES >/dev/null
 sysrc vm_dir="zfs:zroot/vm" >/dev/null
 sysrc postgresql_enable=YES >/dev/null
-sysrc kea_dhcp4_enable=YES >/dev/null
+sysrc kea_enable=YES >/dev/null
+sysrc -x kea_dhcp4_enable >/dev/null 2>&1 || true
 sysrc -x kea_ctrl_agent_enable >/dev/null 2>&1 || true
 sysrc pf_enable=YES >/dev/null
 sysrc pflog_enable=YES >/dev/null
